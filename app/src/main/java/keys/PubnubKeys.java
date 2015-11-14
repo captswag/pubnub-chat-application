34f897a8b147d045fc10d37549c8282cc48026d2
4k3R <iamanjithsasindran@gmail.com>
2015-11-14 17:22:15 +0400

    Added screenshot and removed keys

--- app/src/main/java/keys/PubnubKeys.java
+++ app/src/main/java/keys/PubnubKeys.java
@@ -7,7 +7,7 @@ package keys;
 public class PubnubKeys {
 
     public static final String PUBLISH_KEY = "pub-x-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
-    public static final String SUBSCRIBE_KEY = "sub-x-xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx";
+    public static final String SUBSCRIBE_KEY = "sub-x-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
     public static final String CHANNEL_NAME = "personal_channel";
 
 }
